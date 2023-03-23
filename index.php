cfe8036d630ec7175f06e62f44eef4e10e78647d
rajendrakrshaw <rajendra250601@gmail.com>
2023-03-23 20:10:44 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="roomForm">
+        <form action="ceateRoom.php" method="post">
+            chuglikaro.com/ 
+            <input type="text" name="roomName" >
+            <input type="submit" value="createRoom" name="submit">
+        </form>
+    </div>
+</body>
+</html>
